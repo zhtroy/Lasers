29a375f81fcc6bda5df68f5988708a19ba5b5855
zhtroy <zhanggaotai@gmail.com>
2016-10-12 11:32:23 +0800

    add native share

--- Assets/Scripts/ShareButton.cs
+++ Assets/Scripts/ShareButton.cs
@@ -2,8 +2,28 @@
 using System.Collections;
 
 public class ShareButton : MonoBehaviour {
+	private NativeShare shareTool;
 
+
+	void Start(){
+		shareTool = GetComponent<NativeShare> ();
+	}
 	public void ShareToSNS(){
-		Application.CaptureScreenshot ("screenShot.png");
+		string shareText;
+		if (Application.systemLanguage == SystemLanguage.Chinese) {
+			shareText = "我在#Lasers#里达到了" + GameManager.instance.score + "分，你也来试试吧。";
+		} else {
+			shareText = "I scored " + GameManager.instance.score + " in the game #Lasers#";
+		}
+		string url;
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			url = "https://www.apple.com";
+		} else if (Application.platform == RuntimePlatform.Android) {
+			url = "https://www.baidu.com";
+		} else {
+			url = "";
+		}
+		shareTool.ShareScreenshotWithTextAndUrl (shareText, url);
+			
 	}
 }
